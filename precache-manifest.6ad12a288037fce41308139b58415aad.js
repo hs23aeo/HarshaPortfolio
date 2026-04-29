11a8bc58adaa79ddf005afa239ef0cc13179aeca
Foxtrot123-png <66312252+Foxtrot123-png@users.noreply.github.com>
2026-04-29 19:06:04 +0100

    Updates

---
+++ precache-manifest.6ad12a288037fce41308139b58415aad.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7973e30be1856dee82496abf2fbdc9a",
+    "url": "/HarshaPortfolio/index.html"
+  },
+  {
+    "revision": "4215e3e8e0678ba8dabe",
+    "url": "/HarshaPortfolio/static/css/main.aaf76062.chunk.css"
+  },
+  {
+    "revision": "56db307aa915c98f21e3",
+    "url": "/HarshaPortfolio/static/js/2.684a89d4.chunk.js"
+  },
+  {
+    "revision": "4215e3e8e0678ba8dabe",
+    "url": "/HarshaPortfolio/static/js/main.b2c4e299.chunk.js"
+  },
+  {
+    "revision": "3ddc504b85a04bf6de83",
+    "url": "/HarshaPortfolio/static/js/runtime-main.18aa3123.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/HarshaPortfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/HarshaPortfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/HarshaPortfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/HarshaPortfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/HarshaPortfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/HarshaPortfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/HarshaPortfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/HarshaPortfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6211df5d0a4274df48af89562cf31ae8",
+    "url": "/HarshaPortfolio/static/media/PROFILKE_.6211df5d.jpg"
+  },
+  {
+    "revision": "9af7b85da11bb4ab825aca1d66051f50",
+    "url": "/HarshaPortfolio/static/media/University_of_Hertfordshire_logo.9af7b85d.png"
+  },
+  {
+    "revision": "56aa9a726e4ab7065cef653f1019db6d",
+    "url": "/HarshaPortfolio/static/media/address_image.56aa9a72.svg"
+  },
+  {
+    "revision": "eaf02c2d0158b6b4b8c2a0a0cf830a04",
+    "url": "/HarshaPortfolio/static/media/animated_ashutosh.eaf02c2d.png"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/HarshaPortfolio/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/HarshaPortfolio/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "6b86e9c27c51c4ddf6a79df33301b2c9",
+    "url": "/HarshaPortfolio/static/media/blogs_image_female.6b86e9c2.svg"
+  },
+  {
+    "revision": "356e6523c7ce8e0a3ba7d523e6a3ac89",
+    "url": "/HarshaPortfolio/static/media/cloud_infrastructure.356e6523.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/HarshaPortfolio/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/HarshaPortfolio/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "178e784cfb5bd9efdd4cc20ba094fb3f",
+    "url": "/HarshaPortfolio/static/media/data_science.178e784c.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/HarshaPortfolio/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/HarshaPortfolio/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/HarshaPortfolio/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "b7f9e39789476e377e12b736faa02745",
+    "url": "/HarshaPortfolio/static/media/experience.b7f9e397.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/HarshaPortfolio/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/HarshaPortfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/HarshaPortfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/HarshaPortfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/HarshaPortfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/HarshaPortfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/HarshaPortfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/HarshaPortfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/HarshaPortfolio/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/HarshaPortfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/HarshaPortfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/HarshaPortfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/HarshaPortfolio/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/HarshaPortfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/HarshaPortfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c446a1938610656270b6ba7deaad909b",
+    "url": "/HarshaPortfolio/static/media/feelingProud.c446a193.png"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/HarshaPortfolio/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/HarshaPortfolio/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/HarshaPortfolio/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/HarshaPortfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "90ff5c2b042b3ced9143c395419bd676",
+    "url": "/HarshaPortfolio/static/media/herts_logo.90ff5c2b.webp"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/HarshaPortfolio/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "4eb38f1fe184a282a9132c1ef36dbe40",
+    "url": "/HarshaPortfolio/static/media/icsi_logo.4eb38f1f.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/HarshaPortfolio/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/HarshaPortfolio/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/HarshaPortfolio/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/HarshaPortfolio/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/HarshaPortfolio/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/HarshaPortfolio/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/HarshaPortfolio/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "43138f83989d2b5f2e9ee95263ed521b",
+    "url": "/HarshaPortfolio/static/media/ni_logo.43138f83.jpg"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/HarshaPortfolio/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/HarshaPortfolio/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5dead07629581e6c4eec6da5678e3c5b",
+    "url": "/HarshaPortfolio/static/media/pret.5dead076.png"
+  },
+  {
+    "revision": "def1654d2eb05b6caeb229f959584d10",
+    "url": "/HarshaPortfolio/static/media/pret.def1654d.jpg"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/HarshaPortfolio/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/HarshaPortfolio/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "f8c389100485d2bea49a334af9c91c7c",
+    "url": "/HarshaPortfolio/static/media/projects_image_female.f8c38910.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/HarshaPortfolio/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "79b2dade5c7be355db59474e2e448d94",
+    "url": "/HarshaPortfolio/static/media/qfeelingProud.79b2dade.svg"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/HarshaPortfolio/static/media/qfeelingProud_1.2936ce95.svg"
+  },
+  {
+    "revision": "fbde40f858a6f2b55c74e26fbcdf9bcc",
+    "url": "/HarshaPortfolio/static/media/sndt_logo.fbde40f8.png"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/HarshaPortfolio/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/HarshaPortfolio/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/HarshaPortfolio/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/HarshaPortfolio/static/media/tiktok_logo.0993e324.png"
+  }
+]);
